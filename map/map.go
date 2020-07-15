2121467d4cbf7f0e5ae085a53114a3849ab7364a
Kumar Ramanathan <5899848+kramanathan01@users.noreply.github.com>
2020-07-14 18:41:20 -0700

    Significant refactoring
    Signed-off-by: Kumar Ramanathan <5899848+kramanathan01@users.noreply.github.com>

--- map/map.go
+++ map/map.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -16,67 +17,82 @@ import (
 	"urlshort"
 )
 
+var (
+	home, mapFile string
+)
+
 func main() {
+
+	// mux := defaultMux()
+
+	// pathUrls, err := parseJSON(mapFile)
+	// if err != nil {
+	// 	log.Panicf("Error getting map JSON: %v", err)
+	// }
+
+	// mapHandler := urlshort.MapHandler(pathUrls, mux)
+
+	handler := setHandler()
+
+	// Start Server
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		ReadTimeout:  120 * time.Second,
+		WriteTimeout: 120 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      handler,
+	}
+	go startServer(srv)
+
+	//wait for signal
+	err := signalWait(srv, mapFile)
+	if err != nil {
+		log.Fatalf("Failed to shutdown server %v", err)
+	}
+}
+
+func init() {
 	// READ json map from ~/.map/map.json
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
-	home := user.HomeDir
-	mapFile := filepath.Join(home, ".map.json")
-	logFile := filepath.Join(home, ".map.log")
-
-	// Enable Logging
-	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	log.SetOutput(file)
-
-	mux := defaultMux()
-
-	jsonBlob, err := getContent(mapFile)
-	if err != nil {
-		log.Panicf("Error getting map JSON: %v", err)
-	}
-
-	jsonHandler, err := urlshort.JSONHandler(jsonBlob, mux)
-	if err != nil {
-		log.Panicf("Error in Handler: %v", err)
-	}
-
-	srv := &http.Server{
-		Addr:         "localhost:8080",
-		ReadTimeout:  120 * time.Second,
-		WriteTimeout: 120 * time.Second,
-		IdleTimeout:  120 * time.Second,
-		Handler:      jsonHandler,
-	}
-
-	//Start server and listen
-	go func() {
-		log.Println("Starting the server on :8080")
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			log.Printf("HTTP ListenAndServe: %v", err)
-		}
-	}()
-
-	// Graceful Shutdown on SIGINT or SIGTERM
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	<-quit
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = srv.Shutdown(ctx)
-	if err != nil {
-		log.Fatalf("Failed to shutdown server %v", err)
-	}
-	log.Println("Graceful shutdown of server")
+	home = user.HomeDir
+	mapFile = filepath.Join(home, ".map.json")
 }
 
+func startServer(srv *http.Server) {
+	log.Println("Starting the server on :8080")
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("HTTP ListenAndServe: %v", err)
+	}
+}
+
+func signalWait(srv *http.Server, mfile string) error {
+	// Handle signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGUSR1)
+	for {
+		sig := <-sigs
+		log.Printf("Received %+v", sig)
+
+		switch sig {
+		case syscall.SIGUSR1:
+			log.Println("Reloading config")
+			// pathUrls, err := parseJSON(mfile)
+			// if err != nil {
+			// 	log.Panicf("Error getting map JSON: %v", err)
+			// }
+		case os.Interrupt, syscall.SIGTERM:
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			log.Println("Graceful shutdown of server")
+			return srv.Shutdown(ctx)
+		}
+	}
+
+}
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
@@ -99,4 +115,28 @@ func getContent(file string) ([]byte, error) {
 		return nil, err
 	}
 	return c, nil
+}
+
+func parseJSON(file string) (map[string]string, error) {
+	m := make(map[string]string)
+	jb, err := getContent(file)
+	if err != nil {
+		log.Panicf("Error getting map JSON: %v", err)
+	}
+	err = json.Unmarshal(jb, &m)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func setHandler() http.HandlerFunc {
+	mux := defaultMux()
+
+	pathUrls, err := parseJSON(mapFile)
+	if err != nil {
+		log.Panicf("Error getting map JSON: %v", err)
+	}
+
+	return urlshort.MapHandler(pathUrls, mux)
 }
